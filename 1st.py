7da2849de46eeda0a16fa19de7367a277d312f2e
Aryan Sapsod <111196181+aryansapsod@users.noreply.github.com>
2025-11-21 21:34:29 +0530

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,27 @@
+#Perso_1
+name_1 ="Aryan"
+Age_1 =21
+City_1="Pune"
+
+print(f"Name1={name_1}")
+print(f"Age={Age_1}")
+print(f"City_1={City_1}")
+print("_"*30)
+
+#person_2
+
+name_2 ="Oma"
+Age_2 =21
+City_2="Pune"
+
+print(f"Name1={name_2}")
+print(f"Age={Age_2}")
+print(f"City_1={City_2}")
+print("_"*30)
+
+persom1={"name_2":"Oma","Age_2":21,"City_2":"Pune"}
+
+def person_1(person):
+    print(f"name ={person('name_2')}")
+    print(f"{person}={Age_2}")
+person_1(person_1)
